bb80cd2930105b4fcd1b00625128d2eaef7de9cd
Markus Dürauer <Markus Drauer>
2016-11-13 18:59:35 +0000

    i3 Setup Script skeleton added

---
+++ i3setup/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "----------------------------------------------------------------"
+echo "| i3 Tiling Window Manager Setup of Infoscreen on Raspberry Pi |"
+echo "----------------------------------------------------------------"
+
+echo - Install required packages
+sudo apt-get install i3 lightdm xorg iceweasel
